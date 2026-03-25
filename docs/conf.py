5b5988d7eb1a1228c482c833fc460ddfcaf4bf29
ric-evans <emejqz@gmail.com>
2026-03-25 17:55:36 -0500

    `'navigation_depth': 3,`

--- docs/conf.py
+++ docs/conf.py
@@ -1,34 +1,34 @@
 extensions = [
-    'myst_parser',
-    'sphinx.ext.githubpages',
-    'sphinxcontrib.openapi',
-    'sphinx_rtd_theme',
+    "myst_parser",
+    "sphinx.ext.githubpages",
+    "sphinxcontrib.openapi",
+    "sphinx_rtd_theme",
 ]
 
-root_doc = 'index'
-html_theme = 'sphinx_rtd_theme'
+root_doc = "index"
+html_theme = "sphinx_rtd_theme"
 
-project = 'Event Workflow Management Service'
-author = 'WIPAC Developers'
+project = "Event Workflow Management Service"
+author = "WIPAC Developers"
 html_show_copyright = False
 
-html_title = 'Event Workflow Management Service'
-html_short_title = 'EWMS'
+html_title = "Event Workflow Management Service"
+html_short_title = "EWMS"
 
-html_last_updated_fmt = '%Y-%m-%d %H:%M UTC'
+html_last_updated_fmt = "%Y-%m-%d %H:%M UTC"
 html_last_updated_use_utc = True
 
 exclude_patterns = [
-    '_build',
+    "_build",
 ]
 
-html_static_path = ['_static']
-html_css_files = ['ewms-banner.css', 'ewms-nav.css']
-html_js_files = ['ewms-nav.js']
+html_static_path = ["_static"]
+html_css_files = ["ewms-banner.css", "ewms-nav.css"]
+html_js_files = ["ewms-nav.js"]
 
 html_theme_options = {
-    'collapse_navigation': False,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False,
+    "collapse_navigation": False,
+    "navigation_depth": 3,
+    "includehidden": True,
+    "titles_only": False,
 }
